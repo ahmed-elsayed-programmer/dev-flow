f045e4ee44c618788f4b12729aa6a0802be0150e
Ahmed Elsayed <ahmedelsayed3157@gmail.com>
2025-03-15 06:48:58 +0200

    Implement Edit and Get server actions and implemention

--- database/tag.model.ts
+++ database/tag.model.ts
@@ -1,9 +1,11 @@
-import { model, models, Schema, Types } from "mongoose";
+import { model, models, Schema } from "mongoose";
 
 export interface ITag {
   name: string;
   question: number;
 }
+
+export interface ITagDoc extends ITag, Document {}
 
 const TagSchema = new Schema<ITag>(
   {
--- lib/validations.ts
+++ lib/validations.ts
@@ -125,3 +125,11 @@ export const SignInWithOAuthSchema = z.object({
     image: z.string().url({ message: "Please provide a valid URL." }),
   }),
 });
+
+export const EditQuestionSchema = AskQuestionSchema.extend({
+  questionId: z.string().min(1, { message: "Question ID is required." }),
+});
+
+export const GetQuestionSchema = z.object({
+  questionId: z.string().min(1, { message: "Question ID is required." }),
+});
